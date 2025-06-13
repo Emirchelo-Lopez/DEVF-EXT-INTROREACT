894e88844eee101f1261636510c5cc673c079dce
Emirchelo-Lopez <elopezarmenincome@gmail.com>
2025-06-12 19:21:43 -0600

    Add App component using bootstrap and update main.jsx import style

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    // Example usage in a component:
+    <div className="container">
+      <button className="btn btn-primary">Bootstrap Button</button>
+    </div>
+  );
+}
